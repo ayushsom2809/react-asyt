1fbf9e7b0584546826ecc3880b1294f3740fbdcf
ayushsom2809 <ayushsom10@gmail.com>
2026-09-19 10:07:40 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent1'>
+      <h1 id='child1'>Hello, React!</h1>
+      <h3 id='child2'>whats up</h3>
+    </div>
+    <div id='parent2'>
+      <h1 id='child3'>Hello, React!</h1>
+      <h3 id='child4'>whats up</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
+
